2e643c109d402cd4b5d00b43c68a519f15ca57b8
Palash Hazra <hazrap065@gmail.com>
2025-06-26 04:01:07 +0530

    Add all changes

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,60 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 
-function Profile() {
-  const [profile, setProfile] = useState(null);
-  const [error, setError]= useState(null);
-  const navigate = useNavigate();
+// function Profile() {
+//   const [profile, setProfile] = useState(null);
+//   const [error, setError]= useState(null);
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchProfile=async ()=>{
+//   useEffect(() => {
+//     const fetchProfile=async ()=>{
    
-    const token = localStorage.getItem('token');
+//     const token = localStorage.getItem('token');
 
-    if (!token) {
-      navigate('/');
-      return;
-    }
- try{
-    const response=await fetch('https://drawing-board-1.onrender.com/users/profile', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      },
-    });
-    const data=await response.json();
-    if(response.ok){
-        setProfile(data.user);
-    }
-    else{
-        setError(data.message || 'Failed to fetch profile');
-        navigate('/login');
-    }
-} catch(err ) {
-      setError('An error occured while fetching the profile');
-      navigate('/login');
-    }
+//     if (!token) {
+//       navigate('/');
+//       return;
+//     }
+//  try{
+//     const response=await fetch('https://drawing-board-1.onrender.com/users/profile', {
+//       method: 'GET',
+//       headers: {
+//         'Authorization': `Bearer ${token}`
+//       },
+//     });
+//     const data=await response.json();
+//     if(response.ok){
+//         setProfile(data.user);
+//     }
+//     else{
+//         setError(data.message || 'Failed to fetch profile');
+//         navigate('/login');
+//     }
+// } catch(err ) {
+//       setError('An error occured while fetching the profile');
+//       navigate('/login');
+//     }
 
-};  
+// };  
 
-fetchProfile();
-  }, [navigate]);
+// fetchProfile();
+//   }, [navigate]);
   
-  if(error){
-    return <p style={{color: 'red', textAlign:'center'}}>{error}</p>;
-  }
+//   if(error){
+//     return <p style={{color: 'red', textAlign:'center'}}>{error}</p>;
+//   }
 
-  if(!profile){
-    return <p style={{textAlign: 'center'}}>Loading...</p>;
-  }
+//   if(!profile){
+//     return <p style={{textAlign: 'center'}}>Loading...</p>;
+//   }
 
-  return (
-       <div style={{textAlign: 'center', marginTop: '50px'}}>
+//   return (
+//        <div style={{textAlign: 'center', marginTop: '50px'}}>
         
-        <h1 >Hello ,{profile.name}</h1>
+//         <h1 >Hello ,{profile.name}</h1>
 
-       </div>
-  );
-}
+//        </div>
+//   );
+// }
 
-export default Profile;
+// export default Profile;
