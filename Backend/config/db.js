2342f92dbce97f6934935d6cf6b2541d7f55bd52
Palash Hazra <hazrap065@gmail.com>
2025-06-26 20:00:32 +0530

    Changes in .env file and db.js file

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://PalashUHazra:Board1234@cluster0.scpzfqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("✅ MongoDB Connected");
-    } catch (error) {
-        console.error("❌ MongoDB Connection Failed:", error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
